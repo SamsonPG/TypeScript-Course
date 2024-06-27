7ce95c98acf05c6ffc8b240dad2efd820c081492
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-06-27 20:11:17 +0530

    Update tutorial.ts

--- vanilla-ts/src/tutorial.ts
+++ vanilla-ts/src/tutorial.ts
@@ -32,3 +32,20 @@ console.log(discount);
 
 let haha: string | undefined; //in this even if the variable is undefined , there will be no problem
 
+let bb:any = 'jk'
+let vall:[]=[w,2,]
+
+//challenge 3
+let temperatures: number[] = [32,54,2,32]
+//temperatures.push('hot')
+
+let averageTemperature: number = temperatures.reduce((total,current) => total + current, 0) / temperatures.length
+console.log(averageTemperature);
+
+let fruits: string[] = ['apple','banana','orange']
+let fruit: string = fruits[Math.floor(Math.random() * fruits.length)]
+
+let colors:string[] = ['red','blue']
+//colors.push(true)
+
+let mixedArray: (number | string)[] = [1,2,3,4]
