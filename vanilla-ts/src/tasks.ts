9215d6faab67ddbf7df530e4790a370418a1264d
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-07-02 12:50:30 +0530

    Update tasks.ts

--- vanilla-ts/src/tasks.ts
+++ vanilla-ts/src/tasks.ts
@@ -1,5 +1,16 @@
-const btn= document.querySelector('.test-btn')!;
+const taskForm = document.querySelector<HTMLFormElement>('form');
+const formInput= document.querySelector<HTMLInputElement>('form-input');
+const tasklistElement = document.querySelector<HTMLUListElement>('.list');
 
+
+ type Task={
+    description:string;
+    isCompleted:boolean;
+ };
+
+ const tasks: Task[]=[];
+
+ 
 
 
 
