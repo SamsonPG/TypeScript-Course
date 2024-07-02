8e86c3d8aa703becacf94909487f21f6f8972a45
SamsonPG <121846435+SamsonPG@users.noreply.github.com>
2024-07-02 18:25:18 +0530

    Update tasks.ts

--- vanilla-ts/src/tasks.ts
+++ vanilla-ts/src/tasks.ts
@@ -1,16 +1,57 @@
-const taskForm = document.querySelector<HTMLFormElement>('form');
-const formInput= document.querySelector<HTMLInputElement>('form-input');
-const tasklistElement = document.querySelector<HTMLUListElement>('.list');
+const taskForm = document.querySelector<HTMLFormElement>("form");
+const formInput = document.querySelector<HTMLInputElement>("form-input");
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
 
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
 
- type Task={
-    description:string;
-    isCompleted:boolean;
- };
+const tasks: Task[] = [];
 
- const tasks: Task[]=[];
+//  function createTask(event: SubmitEvent){
+//     event.preventDefault()
+//     const taskDescription = formInput?.value
+//     if(taskDescription){
 
- 
+//         formInput.value='';
+//         return;
 
+//     }
+//     alert('Please enter a task description');
+//  }
+//  taskForm?.addEventListener('submit',createTask)
 
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+  if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    //add task to list
+    addTask(task);
+    //render tasks
+    renderTask(task);
+    //save to local storage
 
+    //update local storage
+
+    formInput.value = "";
+    return;
+  }
+  alert("Please enter a task description");
+});
+
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+  
+}
+
+function renderTask(task:Task):void{
+    const taskElement = document.createElement('li')
+    taskElement.textContent = task.description
+    taskListElement?.appendChild(taskElement)
+}
